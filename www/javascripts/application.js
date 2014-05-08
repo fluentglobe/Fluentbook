e195c42901185032fab7c9f5f9d1fcb7e15df054
Henrik Vendelbo <thepian@thepia.com>
2014-05-08 22:59:27 +0200

    black bg, no navbar

--- www/javascripts/application.js
+++ www/javascripts/application.js
@@ -1,3 +1,5 @@
+// No navigation bar
+// steroids.view.navigationBar.show("Hello World!");
 
-// steroids.view.navigationBar.show("Hello World");
-
+// Set the WebView background color to white (effective on iOS only)
+steroids.view.setBackgroundColor("#000000"); // "#FFFFFF");
