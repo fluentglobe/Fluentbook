3c37d6cbf4773ea9fd7f06ade55c878586a96b70
Henrik Vendelbo <henrik@thepia.com>
2014-06-25 21:55:41 +0200

    removed old view controller

---
+++ CardKit/CardKit/CardKit.h
@@ -0,0 +1,19 @@
+//
+//  CardKit.h
+//  CardKit
+//
+//  Created by Henrik Vendelbo on 24/06/2014.
+//  Copyright (c) 2014 Right Here Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CardKit.
+FOUNDATION_EXPORT double CardKitVersionNumber;
+
+//! Project version string for CardKit.
+FOUNDATION_EXPORT const unsigned char CardKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CardKit/PublicHeader.h>
+
+
---
+++ CardKit/CardKitTests/CardKitTests.swift
@@ -0,0 +1,35 @@
+//
+//  CardKitTests.swift
+//  CardKitTests
+//
+//  Created by Henrik Vendelbo on 24/06/2014.
+//  Copyright (c) 2014 Right Here Inc. All rights reserved.
+//
+
+import XCTest
+
+class CardKitTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
