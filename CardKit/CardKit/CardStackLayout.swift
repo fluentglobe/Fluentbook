802f7857416b32c811219dee895450b3ac6970a6
Henrik Vendelbo <henrik@thepia.com>
2014-06-26 13:17:05 +0200

    compute layout in prepare

--- CardKit/CardKit/CardStackLayout.swift
+++ CardKit/CardKit/CardStackLayout.swift
@@ -15,14 +15,17 @@ class CardStackLayout: UICollectionViewLayout {
     var reveal:CGFloat = 0.0
     var center = CGPoint(x: 160, y: 300)
     var exposedSize = CGSize(width: 320, height: 480)
+    var exposedHeight = CGFloat(0)
     
     var bottomStackGap = 20.0,
         bottomStackHeight = 50.0
     var layoutMargin = UIEdgeInsetsMake(20.0, 0.0, 0.0, 0.0)
     var bounceFactor = 0.2
-    var minReveal:CGFloat = 80.0
+    var minReveal:CGFloat = 35.0
     
     //TODO support multi sections: multiple stacks
+    
+    var attributes = UICollectionViewLayoutAttributes[]()
     
     override func prepareLayout() {
         super.prepareLayout()
@@ -44,7 +47,9 @@ class CardStackLayout: UICollectionViewLayout {
         self.cellCount = self.collectionView.numberOfItemsInSection(0)
         let perCell:CGFloat = viewSize.height / CGFloat(self.cellCount)
         self.reveal = max(self.minReveal, perCell)
+        self.reveal = self.minReveal // testing....
         
+        self.exposedHeight = self.exposedSize.height + self.reveal * CGFloat(self.cellCount - 1)
 
         
         if let cardView = self.collectionView as? CardView {
@@ -52,12 +57,33 @@ class CardStackLayout: UICollectionViewLayout {
             //TODO tweak layoutMargin minReveal
         }
         
+        // pre-calc attributes
+        // placing from the bottom up having the topmost (last in stack) be visually on top
+        if self.cellCount != self.attributes.count {
+            var forAll = UICollectionViewLayoutAttributes[]()
+            for index in 0 .. self.cellCount {
+                var path = NSIndexPath(forItem: index, inSection: 0)
+                
+                let attributes = CardViewLayoutAttributes(forCellWithIndexPath: path)
+                attributes.foreground = (self.exposedItem == path.item)
+                
+                // card has full height whether overlapped or not
+                let centerY = self.exposedHeight - CGFloat(Float(path.item) + 0.5) * self.reveal
+                attributes.center = CGPointMake(self.center.x, centerY - self.exposedSize.height/2 + self.reveal/2)
+                attributes.size = CGSize(width: self.exposedSize.width, height: self.exposedSize.height)
+//                attributes.zIndex = index // last card top of pile
+                
+                forAll.append(attributes)
+                
+            }
+            self.attributes = forAll
+        }
     }
     
     override func collectionViewContentSize() -> CGSize {
         // switch exposed to spread
         if self.exposedItem == nil {
-            let total = CGFloat(self.cellCount) * self.reveal
+            let total = self.exposedHeight//CGFloat(self.cellCount) * self.reveal
             return CGSize(
                 width: self.exposedSize.width + self.layoutMargin.left + self.layoutMargin.right,
                 height: total + self.layoutMargin.top + self.layoutMargin.bottom)
@@ -69,33 +95,23 @@ class CardStackLayout: UICollectionViewLayout {
         
     override func layoutAttributesForElementsInRect(rect: CGRect) -> AnyObject[] {
         
-        let attributes = NSMutableArray()
-        for index in 0 .. self.cellCount-1 {
-            var indexPath = NSIndexPath(forItem: index, inSection: 0)
-            attributes.addObject(self.layoutAttributesForItemAtIndexPath(indexPath))
+        let attrsInRect = NSMutableArray()
+        for attributes in self.attributes {
+            if CGRectIntersectsRect(rect, attributes.frame) {
+                attrsInRect.addObject(attributes)
+            }
         }
-        return attributes
+        return attrsInRect
     }
     
-    override func layoutAttributesForItemAtIndexPath(path:NSIndexPath)
-        -> UICollectionViewLayoutAttributes {
-
-        let attributes = CardViewLayoutAttributes(forCellWithIndexPath: path)
-//        if self.exposedItem is Int {
-            attributes.foreground = (self.exposedItem == path.item)
-//        }
-        
-        // placing from the bottom up having the topmost (last in stack) be visually on top
-
-        // card has full height whether overlapped or not
-        let centerY = CGFloat(Float(self.cellCount) - Float(path.item) - 0.5) * self.reveal
-        attributes.center = CGPointMake(self.center.x, centerY - self.exposedSize.height/2 + self.reveal/2)
-        attributes.size = CGSize(width: self.exposedSize.width, height: self.exposedSize.height)
-        attributes.zIndex = path.item // last card top of pile
-            
-//        println("Laying out item \(path.item), (\(attributes.center.x),\(attributes.center.y))")
-            
-        return attributes
+    override func layoutAttributesForItemAtIndexPath(path:NSIndexPath) -> UICollectionViewLayoutAttributes? {
+        if path.section > 1 {
+            return nil  //TODO multisection support
+        }
+        if path.item >= self.cellCount {
+            return nil
+        }
+        return self.attributes[path.item]
     }
     
     //TODO decorator view support:
